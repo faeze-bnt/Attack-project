bc743f722961e7086d6702df4ff6aeb1327fc7e0
Fazi <banitaba.fzs@gmail.com>
2024-01-23 20:49:16 -0600

    Add files via upload
    Signed-off-by: Fazi <banitaba.fzs@gmail.com>

---
+++ to-run/Confusion_matrix.py
@@ -0,0 +1,35 @@
+
+from sklearn.metrics import confusion_matrix
+import numpy as np
+
+import matplotlib.pyplot as plt
+import seaborn as sns; sns.set()  # For pretty confusion matrix
+
+def one_hot_to_binary(one_hot_array):
+  binary_data = []
+  for i in range(one_hot_array.shape[0]):
+    for j in range(0, 10):
+        if (j == np.argmax(one_hot_array[i])):
+            binary_data.append(j)
+  binary_array = np.array(binary_data)
+  return binary_array
+
+class Conf_matrix:
+  def __init__(self, trues, predictions, plt_file):
+    true_label = one_hot_to_binary(trues)
+    pred_label = one_hot_to_binary(predictions)
+
+    c_matrix = confusion_matrix(true_label, pred_label)
+    # print(c_matrix)
+
+    # drae a plot to visually see the results.
+    fig, ax = plt.subplots()
+    sns.heatmap(c_matrix, annot=False, ax=ax, cmap='Blues')  # Annotate the cells with the numeric values
+
+    ax.set_xlabel('Predicted labels')
+    ax.set_ylabel('True labels')
+    ax.set_title('Confusion Matrix')
+    plt.savefig(plt_file)
+
+
+
--- to-run/Run_This.py
+++ to-run/Run_This.py
@@ -17,7 +17,7 @@ from l2_attack import CarliniL2
 
 from setup_fake_mnist import FakeModel
 from Load_data import Load_DATA
-
+from Confusion_matrix import Conf_matrix
 from test_attack import generate_data, show
 import time
 
@@ -29,7 +29,7 @@ batch_size = 64
 num_classes = 10
 epochs = 50
 train_samples= 60000
-test_samples= 9999 #10000
+test_samples= 10000
 validation_samples = 5000
 params = [20, 20, 800, 10, 10]
 
@@ -52,14 +52,14 @@ def train_main(data, train_1_test_2, is_SC, params):
 
     if is_SC==True:
 
-        model.add(MyConv2D(filters=params[0], kernel=(5, 5), Method = 'Stoch', Width = 8, Sobol_num1 = 1, 
+        model.add(MyConv2D(filters=params[0], kernel=(5, 5), Method = 'Float', Width = 8, Sobol_num1 = 1, 
                            Sobol_num2 = 4, Stream_Length = 16, input_shape=data.input_shape))		#used to be Stoch 
         model.add(MaxPooling2D(pool_size=(2, 2)))
         model.add(MyConv2D(filters=params[1], kernel=(5, 5), Method = 'Float', Width = 8, Sobol_num1 = 2, 
-                           Sobol_num2 = 4, Stream_Length = 1024))			        #used to be Fixed	
+                           Sobol_num2 = 4, Stream_Length = 64))			        #used to be Fixed	
         model.add(MaxPooling2D(pool_size=(2, 2)))
         model.add(Flatten())
-        model.add(MyDense(filters=params[2], Bias=True, Method = 'Float', WidthIn=8, WidthOut=8, Str_Len=16))                                                #used to be Fixed
+        model.add(MyDense(filters=params[2], Bias=True, Method = 'Fixed', WidthIn=8, WidthOut=8, Str_Len=16))                                                #used to be Fixed
         model.add(Activation('relu'))
         model.add(MyDense(filters=params[3], Bias=True, Method = 'Float',  WidthIn=8, WidthOut=8, Str_Len=256))                                                #used to be Fixed
         model.add(Activation('relu'))
@@ -109,12 +109,21 @@ def train_main(data, train_1_test_2, is_SC, params):
         model.load_weights(filepath)
         print("Weigths are loaded from disk")
         test_loss, test_acc = model.evaluate(data.x_test, data.y_test)
-        print("we are in SC:", is_SC)
+        # print("we are in SC:", is_SC)
         print(f'Test accuracy: {test_acc}, Test loss: {test_loss}')
+
+        test_pred = model.predict(data.x_test)
+        plt_name = ""
+        if is_SC:
+          plt_name = "C_matrix_SC.jpg"
+        else:
+          plt_name = "C_matrix_binary.jpg"
+        Conf_matrix(data.y_test, test_pred, plt_name)
+
 
 
     #----for both train and inference	
-    print('batch_size',batch_size,'epochs',epochs)	  
+    # print('batch_size',batch_size,'epochs',epochs)	  
     #score = model.evaluate(x_test, y_test, verbose=0)
     #print('Test loss:', score[0])
     #print('Test accuracy:', score[1])
@@ -129,7 +138,7 @@ def attack_main():
         # model defenition moved to class: FakeModel
         model_fake = FakeModel(model_name, num_classes, params, sess)
         attack = CarliniL2(sess, model_fake, batch_size=9, max_iterations=1000, confidence=0)
-        inputs, targets = generate_data(data_set, samples=int(test_samples/9), targeted=False, #True, 
+        inputs, targets = generate_data(data_set, samples=test_samples, targeted=True, 
                                         start=0, inception=False)
         
         timestart = time.time()
@@ -137,13 +146,14 @@ def attack_main():
         timeend = time.time()
         
         print("Took",timeend-timestart,"seconds to run ",len(inputs),"samples.")
+        print("Total number of adv samples are: ", len(adv))
 
-        print(len(adv))
+        # print(len(adv))
         for i in range(5):
-            # print("Valid:")
-            # show(inputs[i])
-            # print("Adversarial:")
-            # show(adv[i])
+            print("Valid:")
+            show(inputs[i])
+            print("Adversarial:")
+            show(adv[i])
             
             print("Classification:", model_fake.model.predict(adv[i:i+1]))
             print("Total distortion:", np.sum((adv[i]-inputs[i])**2)**.5)
@@ -161,9 +171,9 @@ print("done 1 : data loaded -----------------------")
 # train_main(data_set, train_1_test_2=1, is_SC=False, params=params)
 # print("done 3 : nn trained -----------------------")
 
-#test the stochastic model
-train_main(data_set, train_1_test_2=2, is_SC=True, params=params)
-print("done 4 : SC tested -----------------------")
+# #test the stochastic model
+# train_main(data_set, train_1_test_2=2, is_SC=True, params=params)
+# print("done 4 : SC tested -----------------------")
 
 # #test binary model now
 # train_main(data_set, train_1_test_2=2, is_SC=False, params=params)
@@ -177,14 +187,20 @@ print("done 4 : SC tested -----------------------")
 # data_set.save_after_attack(adv_test_data, adv_test_labels)
 # print("done 9 : attack saved in file -----------------------")
 
-# data_set.load_after_attack()
-# print("done 10 : attacked data loaded from file -----------------------")
+data_set.load_after_attack()
+print("done 10 : attacked data loaded from file -----------------------")
 
-# #test SC network, after attack being applied
-# train_main(data_set, train_1_test_2=2, is_SC=True, params=params)
-# print("done 7 : SC tested after attack -----------------------")
+#test SC network, after attack being applied
+train_main(data_set, train_1_test_2=2, is_SC=True, params=params)
+print("done 7 : SC tested after attack -----------------------")
 
 
 # #test binary network, after attack being applied
 # train_main(data_set, train_1_test_2=2, is_SC=False, params=params)
 # print("done 8 : nn tested after attack -----------------------")
+
+
+
+
+
+
